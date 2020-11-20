27bed98286f43ac412f822cfa897904e773805d8
zhux002 <xingqi.zhu@mail.mcgill.ca>
2020-11-20 17:55:54 -0500

    hypothesis testing t test and boxplot

--- BIOL111Experiment.m
+++ BIOL111Experiment.m
@@ -25,47 +25,75 @@ error_data_cucumbergroup_cucumber = std(data_cucumbergroup_cucumber)/sqrt(numel(
 error_data_mixedgroup_lettuce = std (data_mixedgroup_lettuce)/sqrt(numel(data_mixedgroup_lettuce));
 error_data_mixedgroup_cucumber = std (data_mixedgroup_cucumber)/sqrt(numel(data_mixedgroup_cucumber));
 
-%Lettuce group
-errorbar([2,4],[mean_data_lettucegroup_lettuce,mean_data_lettucegroup_cucumber], ...
-    [ error_data_lettucegroup_lettuce,error_data_lettucegroup_cucumber],'vertical','o');
-
-xlim([0 6]);
-ylim([0 2.5]);
-
-title('Lettuce group');
-xlabel('Food source (left: lettuce, right:cucumbers)')
-ylabel('Amount consumed (gram)')
-
-%Cucumber group
+% %Lettuce group
+% errorbar([2,4],[mean_data_lettucegroup_lettuce,mean_data_lettucegroup_cucumber], ...
+%     [ error_data_lettucegroup_lettuce,error_data_lettucegroup_cucumber],'vertical','o');
+% 
+% xlim([0 6]);
+% ylim([0 2.5]);
+% 
+% title('Lettuce group');
+% xlabel('Food source (left: lettuce, right:cucumbers)')
+% ylabel('Amount consumed (gram)')
+% 
+% %Cucumber group
+% 
+% figure
+% 
+% errorbar([2,4],[mean_data_cucumbergroup_lettuce,mean_data_cucumbergroup_cucumber], ...
+%     [ error_data_cucumbergroup_lettuce,error_data_cucumbergroup_cucumber],'vertical','o');
+% 
+% xlim([0 6]);
+% ylim([0 2.5]);
+% 
+% title('Cucumber group');
+% 
+% title('Cucumber group');
+% xlabel('Food source (left: lettuce, right:cucumbers)')
+% ylabel('Amount consumed (gram)')
+% 
+% %Mixed group
+% 
+% figure
+% 
+% errorbar([2,4],[mean_data_mixedgroup_lettuce,mean_data_mixedgroup_cucumber], ...
+%     [ error_data_mixedgroup_lettuce,error_data_mixedgroup_cucumber],'vertical','o');
+% 
+% xlim([0 6]);
+% ylim([0 2.5]);
+% 
+% title('Mixed group'); 
+% 
+% title('Mixed group');
+% xlabel('Food source (left: lettuce, right:cucumbers)')
+% ylabel('Amount consumed (gram)')
 
 figure
-
-errorbar([2,4],[mean_data_cucumbergroup_lettuce,mean_data_cucumbergroup_cucumber], ...
-    [ error_data_cucumbergroup_lettuce,error_data_cucumbergroup_cucumber],'vertical','o');
-
-xlim([0 6]);
-ylim([0 2.5]);
-
-title('Cucumber group');
-
-title('Cucumber group');
-xlabel('Food source (left: lettuce, right:cucumbers)')
+boxplot([data_lettucegroup_lettuce.',data_lettucegroup_cucumber.'],'Labels',{'Lettuce','Cucumbers'})
+title('Consumption of vegetables for a 24 hour period for Lettuce Group')
+xlabel('Food type')
 ylabel('Amount consumed (gram)')
-
-%Mixed group
 
 figure
-
-errorbar([2,4],[mean_data_mixedgroup_lettuce,mean_data_mixedgroup_cucumber], ...
-    [ error_data_mixedgroup_lettuce,error_data_mixedgroup_cucumber],'vertical','o');
-
-xlim([0 6]);
-ylim([0 2.5]);
-
-title('Mixed group'); 
-
-title('Mixed group');
-xlabel('Food source (left: lettuce, right:cucumbers)')
+boxplot([data_cucumbergroup_lettuce.',data_cucumbergroup_cucumber.'],'Labels',{'Lettuce','Cucumbers'})
+title('Consumption of vegetables for a 24 hour period for Cucumber Group')
+xlabel('Food type')
 ylabel('Amount consumed (gram)')
+
+figure
+x = [data_mixedgroup_lettuce.';data_mixedgroup_cucumber.'];
+g1 = repmat({'Lettuce'},3,1);
+g2 = repmat({'Cucumbers'},4,1);
+g = [g1; g2];
+
+boxplot(x, g)
+
+title('Consumption of vegetables for a 24 hour period for Mixed Diet Group')
+xlabel('Food type')
+ylabel('Amount consumed (gram)')
+
+[h_l,sig_l,ci_l] = ttest2(data_lettucegroup_lettuce,data_lettucegroup_cucumber);
+[h_c,sig_c,ci_c] = ttest2(data_cucumbergroup_lettuce,data_cucumbergroup_cucumber);
+[h_m,sig_m,ci_m] = ttest2(data_mixedgroup_lettuce,data_mixedgroup_cucumber);
 
 
