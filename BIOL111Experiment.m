6d65725495a5660a3dfe563fa252e3ede797bd6c
zhux002 <xingqi.zhu@mail.mcgill.ca>
2021-01-13 14:49:55 -0500

    Final changes and figures

--- BIOL111Experiment.m
+++ BIOL111Experiment.m
@@ -1,6 +1,7 @@
-%% Raffles 
 
 %{
+%% Raffles 
+
 data_lettucegroup_lettuce = 3.75 - [3.72,3.03,2.36,3.47];
 data_lettucegroup_cucumber = 3.75 - [3.60,2.43,2.72,2.67];
 data_cucumbergroup_lettuce = 3.75 - [2.83,1.97,0.95,1.91];
@@ -72,34 +73,48 @@ error_data_mixedgroup_cucumber = std (data_mixedgroup_cucumber)/sqrt(numel(data_
 % ylabel('Amount consumed (gram)')
 
 figure
-boxplot([data_lettucegroup_lettuce.',data_lettucegroup_cucumber.'],'Labels',{'Lettuce','Cucumber'})
-title('Consumption of vegetables for a 24 hour period for Lettuce Group')
+boxplot([data_lettucegroup_lettuce.',data_lettucegroup_cucumber.'],'Labels',{'Cabbage','Cucumber'})
+title('Consumption of vegetables for a 24 hour period for Cabbage Group','FontSize', 18)
 xlabel('Food type')
 ylabel('Amount consumed (gram)')
 
+ax = gca
+ax.XAxis.FontSize = 18;
+ax.YAxis.FontSize = 18;
+
 figure
-boxplot([data_cucumbergroup_lettuce.',data_cucumbergroup_cucumber.'],'Labels',{'Lettuce','Cucumber'})
-title('Consumption of vegetables for a 24 hour period for Cucumber Group')
+boxplot([data_cucumbergroup_lettuce.',data_cucumbergroup_cucumber.'],'Labels',{'Cabbage','Cucumber'})
+title('Consumption of vegetables for a 24 hour period for Cucumber Group','FontSize', 18)
 xlabel('Food type')
 ylabel('Amount consumed (gram)')
 
 figure
 x = [data_mixedgroup_lettuce.';data_mixedgroup_cucumber.'];
-g1 = repmat({'Lettuce'},3,1);
+g1 = repmat({'Cabbage'},3,1);
 g2 = repmat({'Cucumber'},4,1);
 g = [g1; g2];
 
 boxplot(x, g)
 
-title('Consumption of vegetables for a 24 hour period for Mixed Diet Group')
+ax = gca
+ax.XAxis.FontSize = 18;
+ax.YAxis.FontSize = 18;
+
+title('Consumption of vegetables for a 24 hour period for Mixed Diet Group','FontSize', 18)
 xlabel('Food type')
 ylabel('Amount consumed (gram)')
 
-[h_l,sig_l,ci_l] = ttest2(data_lettucegroup_lettuce,data_lettucegroup_cucumber);
-[h_c,sig_c,ci_c] = ttest2(data_cucumbergroup_lettuce,data_cucumbergroup_cucumber);
-[h_m,sig_m,ci_m] = ttest2(data_mixedgroup_lettuce,data_mixedgroup_cucumber);
+[h_l,sig_l,ci_l] = ttest2(data_lettucegroup_lettuce,data_lettucegroup_cucumber,'Vartype','unequal');
+[h_c,sig_c,ci_c] = ttest2(data_cucumbergroup_lettuce,data_cucumbergroup_cucumber,'Vartype','unequal');
+[h_m,sig_m,ci_m] = ttest2(data_mixedgroup_lettuce,data_mixedgroup_cucumber,'Vartype','unequal');
+
+ax = gca
+ax.XAxis.FontSize = 18;
+ax.YAxis.FontSize = 18;
 
 %}
+
+%
 %% Camille 
 
 data_lettucegroup_lettuce = 3.75 - [0.39,0.47,0.51,0.45];
@@ -115,43 +130,119 @@ mean_data_lettucegroup_cucumber = mean(data_lettucegroup_cucumber);
 mean_data_cucumbergroup_lettuce = mean (data_cucumbergroup_lettuce);
 mean_data_cucumbergroup_cucumber = mean(data_cucumbergroup_cucumber);
 
-mean_data_mixedgroup_lettuce = std (data_mixedgroup_lettuce);
-mean_data_mixedgroup_cucumber = std (data_mixedgroup_cucumber);
+mean_data_mixedgroup_lettuce = mean (data_mixedgroup_lettuce);
+mean_data_mixedgroup_cucumber = mean (data_mixedgroup_cucumber);
 
 
+error_data_lettucegroup_lettuce = std (data_lettucegroup_lettuce)/sqrt(numel(data_lettucegroup_lettuce));
+error_data_lettucegroup_cucumber = std(data_lettucegroup_cucumber)/sqrt(numel(data_lettucegroup_cucumber));
 
-% error_data_lettucegroup_lettuce = std (data_lettucegroup_lettuce)/sqrt(numel(data_lettucegroup_lettuce));
-% error_data_lettucegroup_cucumber = std(data_lettucegroup_cucumber)/sqrt(numel(data_lettucegroup_cucumber));
-% 
-% error_data_cucumbergroup_lettuce = std (data_cucumbergroup_lettuce)/sqrt(numel(data_cucumbergroup_lettuce));
-% error_data_cucumbergroup_cucumber = std(data_cucumbergroup_cucumber)/sqrt(numel(data_cucumbergroup_cucumber));
-% 
-% error_data_mixedgroup_lettuce = std (data_mixedgroup_lettuce)/sqrt(numel(data_mixedgroup_lettuce));
-% error_data_mixedgroup_cucumber = std (data_mixedgroup_cucumber)/sqrt(numel(data_mixedgroup_cucumber));
+error_data_cucumbergroup_lettuce = std (data_cucumbergroup_lettuce)/sqrt(numel(data_cucumbergroup_lettuce));
+error_data_cucumbergroup_cucumber = std(data_cucumbergroup_cucumber)/sqrt(numel(data_cucumbergroup_cucumber));
+
+error_data_mixedgroup_lettuce = std (data_mixedgroup_lettuce)/sqrt(numel(data_mixedgroup_lettuce));
+error_data_mixedgroup_cucumber = std (data_mixedgroup_cucumber)/sqrt(numel(data_mixedgroup_cucumber));
 
 figure
 boxplot([data_lettucegroup_lettuce.',data_lettucegroup_cucumber.'],'Labels',{'Cabbage','Cucumber'})
-title('Consumption of vegetables for a 24 hour period for Cabbage Group')
+title('Consumption of vegetables for a 24 hour period for Cabbage Group','FontSize', 18)
 xlabel('Food type')
 ylabel('Amount consumed (gram)')
+
+ax = gca
+ax.XAxis.FontSize = 18;
+ax.YAxis.FontSize = 18;
 
 figure
 boxplot([data_cucumbergroup_lettuce.',data_cucumbergroup_cucumber.'],'Labels',{'Cabbage','Cucumber'})
-title('Consumption of vegetables for a 24 hour period for Cucumber Group')
+title('Consumption of vegetables for a 24 hour period for Cucumber Group','FontSize', 18)
 xlabel('Food type')
 ylabel('Amount consumed (gram)')
 
+ax = gca
+ax.XAxis.FontSize = 18;
+ax.YAxis.FontSize = 18;
+
 figure
 boxplot([data_mixedgroup_lettuce.',data_mixedgroup_cucumber.'],'Labels',{'Cabbage','Cucumber'})
-title('Consumption of vegetables for a 24 hour period for Mixed Diet Group')
+title('Consumption of vegetables for a 24 hour period for Mixed Diet Group','FontSize', 18)
 xlabel('Food type')
 ylabel('Amount consumed (gram)')
+
+ax = gca
+ax.XAxis.FontSize = 18;
+ax.YAxis.FontSize = 18;
 
 [h_l,sig_l,ci_l] = ttest2(data_lettucegroup_lettuce,data_lettucegroup_cucumber);
 [h_c,sig_c,ci_c] = ttest2(data_cucumbergroup_lettuce,data_cucumbergroup_cucumber);
 [h_m,sig_m,ci_m] = ttest2(data_mixedgroup_lettuce,data_mixedgroup_cucumber);
 
 
+%}
+%{
 %% Cassia
+
+data_lettucegroup_lettuce = zeros(1,3);
+data_lettucegroup_cucumber = [0.9,0.36,1.58];
+data_cucumbergroup_lettuce =  zeros(1,3);
+data_cucumbergroup_cucumber = [1.38,0.78,1.67];
+data_mixedgroup_lettuce = zeros(1,3);
+data_mixedgroup_cucumber =  [1.57,0.86,1.84];
+
+mean_data_lettucegroup_lettuce = mean (data_lettucegroup_lettuce);
+mean_data_lettucegroup_cucumber = mean(data_lettucegroup_cucumber);
+
+mean_data_cucumbergroup_lettuce = mean (data_cucumbergroup_lettuce);
+mean_data_cucumbergroup_cucumber = mean(data_cucumbergroup_cucumber);
+
+mean_data_mixedgroup_lettuce = mean (data_mixedgroup_lettuce);
+mean_data_mixedgroup_cucumber = mean (data_mixedgroup_cucumber);
+
+
+error_data_lettucegroup_lettuce = std (data_lettucegroup_lettuce)/sqrt(numel(data_lettucegroup_lettuce));
+error_data_lettucegroup_cucumber = std(data_lettucegroup_cucumber)/sqrt(numel(data_lettucegroup_cucumber));
+
+error_data_cucumbergroup_lettuce = std (data_cucumbergroup_lettuce)/sqrt(numel(data_cucumbergroup_lettuce));
+error_data_cucumbergroup_cucumber = std(data_cucumbergroup_cucumber)/sqrt(numel(data_cucumbergroup_cucumber));
+
+error_data_mixedgroup_lettuce = std (data_mixedgroup_lettuce)/sqrt(numel(data_mixedgroup_lettuce));
+error_data_mixedgroup_cucumber = std (data_mixedgroup_cucumber)/sqrt(numel(data_mixedgroup_cucumber));
+
+figure
+boxplot([data_lettucegroup_lettuce.',data_lettucegroup_cucumber.'],'Labels',{'Carrot','Cucumber'})
+title('Consumption of vegetables for a 24 hour period for Carrot Group','FontSize', 18)
+xlabel('Food type')
+ylabel('Amount consumed (gram)')
+
+ax = gca
+ax.XAxis.FontSize = 18;
+ax.YAxis.FontSize = 18;
+
+figure
+boxplot([data_cucumbergroup_lettuce.',data_cucumbergroup_cucumber.'],'Labels',{'Carrot','Cucumber'})
+title('Consumption of vegetables for a 24 hour period for Cucumber Group','FontSize', 18)
+xlabel('Food type')
+ylabel('Amount consumed (gram)')
+
+ax = gca
+ax.XAxis.FontSize = 18;
+ax.YAxis.FontSize = 18;
+
+figure
+boxplot([data_mixedgroup_lettuce.',data_mixedgroup_cucumber.'],'Labels',{'Carrot','Cucumber'})
+title('Consumption of vegetables for a 24 hour period for Mixed Diet Group','FontSize', 18)
+xlabel('Food type')
+ylabel('Amount consumed (gram)')
+
+ax = gca
+ax.XAxis.FontSize = 18;
+ax.YAxis.FontSize = 18;
+
+[h_l,sig_l,ci_l] = ttest2(data_lettucegroup_lettuce,data_lettucegroup_cucumber);
+[h_c,sig_c,ci_c] = ttest2(data_cucumbergroup_lettuce,data_cucumbergroup_cucumber);
+[h_m,sig_m,ci_m] = ttest2(data_mixedgroup_lettuce,data_mixedgroup_cucumber);
+
+
+%}
 
 
